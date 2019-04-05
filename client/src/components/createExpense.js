68db61cad46afc1812440210475fedc830d502b4
Lexii <37764077+lexiilamb@users.noreply.github.com>
2019-04-05 15:03:09 -0400

    oops2

--- client/src/components/createExpense.js
+++ client/src/components/createExpense.js
@@ -230,6 +230,7 @@ export default class CreateExpense extends Component {
 						options={optionsCategory}
 						onChange={this.onChangeCategory}
 					  />
+					</div>
 					<div className="form-group">
 					  <label>Month: </label>
 					  <Select
