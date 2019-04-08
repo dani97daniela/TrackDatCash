341b9238386da454df24c01ad547ae3e6b7302de
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-08 12:38:42 -0400

    test
    just added a line

--- client/src/App.js
+++ client/src/App.js
@@ -66,6 +66,7 @@ class App extends Component {
 			  <PrivateRoute exact path="/group" component={Group} />
             </Switch>
           </div>
+		  
         </Router>
       </Provider>
     );
