eca15b5fda627562ef8bcc5ed08f604fbef5eabc
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-08 22:01:58 -0400

    Update Register.js
    checking errors

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -53,11 +53,11 @@ class Register extends Component {
     const { errors } = this.state;
 
     return (
-     <div className="App">
+      <div className="App">
 					
 		<div className = "body">
 			<center><h1><img src = {logo2} width = "400" height = "80" class = "rounded" alt = ""/></h1></center>
-          <div className="col s8 offset-s2">
+          
             <Link to="/" className="btn-flat waves-effect">
               <i className="material-icons left">keyboard_backspace</i> Back to
               home
@@ -144,7 +144,6 @@ class Register extends Component {
             </form>
           </div>
         </div>
-	
       </div>
     );
   }
