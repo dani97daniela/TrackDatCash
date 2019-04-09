cc2afff6840cbec553d7795731798453a1be2964
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-08 21:35:57 -0400

    Update Login.js
    just adding more things to see the changes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -58,8 +58,8 @@ class Login extends Component {
       <div className="App">
         <div className = "body">
 			<center><h1><img src = {logo2} width = "400" height = "80" class = "rounded" alt = ""/></h1></center>
-		<div style={{ marginTop: "4rem" }} className="row">
-          <div className="col s8 offset-s2">
+		
+         
             <Link to="/" className="btn-flat waves-effect">
               <i className="material-icons left">keyboard_backspace</i> Back to
               home
@@ -122,8 +122,8 @@ class Login extends Component {
                 </button>
               </div>
             </form>
-          </div>
-        </div>
+        
+        
 		</div>
       </div>
     );
