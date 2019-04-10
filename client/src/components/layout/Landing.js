378246c52d9200b9ceb375397491765d7809bca0
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-10 12:54:08 -0400

    Update Landing.js
    ughhhhhhh

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -21,8 +21,10 @@ class Landing extends Component {
 						  <Link to="register" className="nav-link">Register</Link>
 						</li>
 						</ul>
+					</div>
+					</nav>
 				</div>
-				<div className = "Picture">
+					<div className = "Picture">
 						<h1><img src = {picture} width = "1100" height = "300" class = "rounded" alt = ""/></h1>
 					</div>
 	  <div style={{ height: "75vh" }} className="container valign-wrapper">
