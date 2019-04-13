2f0d25ef5eaac45d9aaf8b88bb41105c56744022
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-04-13 14:26:14 -0400

    Clean up
    Removed old login/register routes from server.js.
    
    Removed /dashboard from /src/components.
    
    Removed /dashboard call from App.js.
    
    Removed loginUser.js (not used).
    
    Removed createUser.js (not used).
    
    Removed appNotUsed,js.

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,6 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
