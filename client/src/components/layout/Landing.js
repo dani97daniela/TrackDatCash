9e1e9420c7b529cee04b723e56ad74147197f96b
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-14 12:27:29 -0400

    Update Landing.js
    trying to add carousel to it now

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -11,6 +11,9 @@ class Landing extends Component {
     return (
       	
 		<div className= "App">
+			
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"></link>
 				<div className="container">
 					
@@ -30,6 +33,42 @@ class Landing extends Component {
 				</div>
 					<div className = "Picture">
 						<center><h1><img src = {picture} width = "1100" height = "300" class = "rounded" alt = ""/></h1></center>
+					</div>
+					<div className = "Real_pics">
+					  <h2>Carousel Example</h2>  
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+   
+							<ol class="carousel-indicators">
+								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#myCarousel" data-slide-to="1"></li>
+								<li data-target="#myCarousel" data-slide-to="2"></li>
+							</ol>
+
+    
+							<div class="carousel-inner">
+								<div class="item active">
+									<img src={description} alt="Pic 1" style="width:100%;">
+								</div>
+
+								<div class="item">
+									<img src={logo} alt="Pic 2" style="width:100%;">
+								</div>
+			
+								<div class="item">
+									<img src={logo2} alt="Pic 3" style="width:100%;">
+								</div>
+							</div>
+
+
+						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+						  <span class="glyphicon glyphicon-chevron-left"></span>
+						  <span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							  <span class="glyphicon glyphicon-chevron-right"></span>
+							  <span class="sr-only">Next</span>
+						</a>
+						</div>
 					</div>
 					
            
