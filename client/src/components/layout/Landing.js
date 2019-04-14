d283e4df161bd42a8689545e6e712e3700680a85
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-14 14:05:14 -0400

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -33,13 +33,13 @@ class Landing extends Component {
 					</nav>
 				</div>
 				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
+   
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
     </ol>
 
-    <!-- Wrapper for slides -->
+    
     <div class="carousel-inner" role="listbox">
       <div class="item active">
         <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image">
