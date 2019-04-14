5be8ef5cdfc7b5dc9ebd2f49821b74217472c503
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-14 14:25:58 -0400

    Update Landing.js
    lol this is becoming confusing to me now

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -33,75 +33,54 @@ class Landing extends Component {
 					</nav>
 				</div>
 				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-   
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>
+					<ol class="carousel-indicators">
+					  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					  <li data-target="#myCarousel" data-slide-to="1"></li>
+					</ol>
+						<div class="carousel-inner" role="listbox">
+						  <div class="item active">
+							<img src="https://placehold.it/1200x400?text=IMAGE" alt="Image"/>
+						  </div>
 
-    
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image"/>
-        <div class="carousel-caption">
-          
-        </div>      
-      </div>
-
-      <div class="item">
-        <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image"/>
-        <div class="carousel-caption">
-          
-        </div>      
-      </div>
-    </div>
-
+						  <div class="item">
+							<img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image"/>   
+						  </div>
+						</div>
+					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+					  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					  <span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+					  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					  <span class="sr-only">Next</span>
+					</a>
+				</div>
   
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-</div>
-  
-<div class="container text-center">    
-  <h3>What We Do</h3><br>
-  <div class="row">
-    <div class="col-sm-4">
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"/>
-      <p>Current Project</p>
-    </div>
-    <div class="col-sm-4"> 
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"/>
-      <p>Project 2</p>    
-    </div>
-    <div class="col-sm-4">
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-    </div>
-  </div>
-</div></br>
-
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
-					
-					
-					
-           
-           
-            <br />
-            
-            
-         
-	 </div>
+				<div class="container text-center">    
+				  <h3>What We Do</h3><br>
+				  <div class="row">
+					<div class="col-sm-4">
+					  <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"/>
+					  <p>Current Project</p>
+					</div>
+					<div class="col-sm-4"> 
+					  <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"/>
+					  <p>Project 2</p>    
+					</div>
+					<div class="col-sm-4">
+					  <div class="well">
+					   <p>Some text..</p>
+					  </div>
+					  <div class="well">
+					   <p>Some text..</p>
+					  </div>
+					</div>
+				  </div>
+				</div><br/>
+			</div>
+		<footer class="container-fluid text-center">
+			<p>Footer Text</p>
+		</footer>
     );
   }
 }
