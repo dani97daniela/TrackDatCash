4f8e1546a91d6ee6ac7ec1a012175aedb52cedb7
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-14 16:06:11 -0400

    ugh

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -41,10 +41,11 @@ class Landing extends Component {
 						<center><h1><img src = {picture} width = "1100" height = "300" class = "rounded" alt = ""/></h1></center>
 					</div>
 				 <br />
-				<div className = "footer">
+				
+		</div>
+		<div className = "footer">
 					<center><p> Team 13</p></center>
 					<center><p> Orlando Florida</p></center>
-				</div>
 		</div>
     );
   }
