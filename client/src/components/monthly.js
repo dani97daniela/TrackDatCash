f49691cd2fe19248717ff43e040f7f73bea7001e
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-16 23:02:01 -0400

    Update monthly.js

--- client/src/components/monthly.js
+++ client/src/components/monthly.js
@@ -431,7 +431,7 @@ class TodosList extends Component {
 				</nav>
 				
 			<form onSubmit={this.onSubmit}>
-				<center><label>Current Year:<input type="text" placeholder={this.state.year} className="form-control" value={this.state.year) onChange={this.onChangeYear}/><input type="submit" value="Update" className="btn btn-info" /></label></center>
+				<center><label>Current Year:<input type="text" placeholder={this.state.year} className="form-control" value={this.state.year} onChange={this.onChangeYear}/><input type="submit" value="Update" className="btn btn-info" /></label></center>
 			</form>
 			  <h3><center>{"Expenses for " + this.state.year}</center></h3>
 			  
